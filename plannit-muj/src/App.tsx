9cc9f5717433a847e09c05ab4bcc5fd408f02226
mohithingorani <mohithingorani2003@gmail.com>
2025-09-08 12:29:32 +0530

    small try

--- plannit-muj/src/App.tsx
+++ plannit-muj/src/App.tsx
@@ -4,10 +4,10 @@ import { Toaster } from 'react-hot-toast';
 import { useAuthStore } from '../store/authStore';
 import { Navbar } from '../components/layout/Navbar';
 import { Footer } from '../components/layout/Footer';
-import  HomePage  from '../pages/HomePage';
 import  EventsPage  from '../pages/EventsPage';
 import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
+import  HomePage  from '../pages/HomePage';
 import "./index.css"
 function App() {
   const { theme } = useAuthStore();
